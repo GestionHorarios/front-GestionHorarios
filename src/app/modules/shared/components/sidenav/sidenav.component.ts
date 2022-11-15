af5335f78c1c1afd02b63a1ec053f832ef57196d
juancarcaicedo <juancarcaicedo@unicauca.edu.co>
2022-11-15 05:52:30 -0500

    sidenav

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
+  //variable que ayuda al control de tamaño y diseño responsivo
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
+  //vaiable que controla el menu
+  menuNav=[
+    {name:"Home",route:"home", icon:"home"},
+    {name:"SubirArchivo", route:"home", icon:"unarchive"},
+    {name:"AgregarInfrastructura", route:"home", icon:"add_circle"},
+    {name:"Configurar", route:"home", icon:"brightness_low"},
+    {name:"Agregar Cursos", route:"home", icon:"add"},
+    {name:"Programar  Cursos", route:"home", icon:"calendar_today"}
+
+  ]
+  constructor(media: MediaMatcher) {
+    this.mobileQuery=media.matchMedia('(max-width: 680px)');
+
+   }
 
   ngOnInit(): void {
   }
